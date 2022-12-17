42fc0d48052c82a5969b97eebde28d7fac1d942f
Thomas Spycher <me@tspycher.com>
2022-12-17 12:18:59 +0100

    Limit Scan to Ruuvi Tags
    Read manufacturers Data and only read data from RuuviTags.
    
    Improved Error handling

--- RuuviClient/RuuviClient.ino
+++ RuuviClient/RuuviClient.ino
@@ -9,29 +9,50 @@
 
 float temperature, humidity, pressure;
 bool new_data;
+bool debug = false;
 
 void blePeripheralDiscoveredHandler(BLEDevice central) {
-  Serial.print("Discovered event from: ");
-  Serial.println(central.address());
-
-  if (central.hasAdvertisementData()) {
-    uint8_t value[64];
-    int l = central.advertisementDataLength();
-    central.advertisementData(value, 64);
-
-    int l_payload = (int)value[3];
-    int format = (int)value[7];
-    int payload_start = 8;
-
-
-    if (format == 5) { // Data Format 5 Protocol Specification (RAWv2)
-      // https://github.com/ruuvi/ruuvi-sensor-protocols/blob/master/dataformat_05.md
-      
-      temperature = (int)(((byte)value[payload_start] << 8) | ((byte)value[payload_start + 1])) * 0.005;
-      humidity = (int)(((byte)value[payload_start+2] << 8) | ((byte)value[payload_start + 3])) * 0.0025;
-      pressure = ((int)(((byte)value[payload_start+4] << 8) | ((byte)value[payload_start + 5])) + 50000) / 100;
-      new_data = true;
+  if(central.hasManufacturerData() && central.hasAdvertisementData()) {
+    // Check PDF From: https://www.bluetooth.com/specifications/assigned-numbers/
+    // 0x0499 Ruuvi Innovations Ltd.
+    // Decimal: 4 153
+    uint8_t manufacturer[central.manufacturerDataLength()];
+    central.manufacturerData(manufacturer, central.manufacturerDataLength());
+    if((int)manufacturer[0] == 153 && (int)manufacturer[1] == 4) {
+      Serial.print("Discovered event from RuuviTag with Mac: ");
+      Serial.println(central.address());
+    } else {
+      if (debug) {
+        Serial.print("Advertisement is from an unknown Device with Manufacturer Id: ");
+        Serial.print((int)manufacturer[1], HEX);
+        Serial.print((int)manufacturer[0], HEX);
+        Serial.println();
+      }
+      return;
     }
+  } else {
+    // no manufacturer Data available
+    return;
+  }
+
+  // Load the Advertised Data and store the values
+  int l = central.advertisementDataLength();
+  uint8_t value[l];
+  central.advertisementData(value, l);
+
+  int l_payload = (int)value[3];
+  int format = (int)value[7];
+  int payload_start = 8;
+
+  if (format == 5) { // Data Format 5 Protocol Specification (RAWv2)
+    // https://github.com/ruuvi/ruuvi-sensor-protocols/blob/master/dataformat_05.md
+    temperature = (int)(((byte)value[payload_start] << 8) | ((byte)value[payload_start + 1])) * 0.005; // Celcius
+    humidity = (int)(((byte)value[payload_start+2] << 8) | ((byte)value[payload_start + 3])) * 0.0025; // Percent
+    pressure = ((int)(((byte)value[payload_start+4] << 8) | ((byte)value[payload_start + 5])) + 50000) / 100; // hPa
+    new_data = true;
+  } else {
+    Serial.print("Unknown Data Format from RuuviTag received: ");
+    Serial.println(format);
   }
 }
 
@@ -60,14 +81,13 @@ void setup() {
     while (1);
   }
   new_data = false;
-  Serial.println("Ruuvi Prototype");
+  Serial.println("Ruuvi Prototype to listen for Ruuvi Advertisements and Decode data");
+  Serial.println("------------------------------------------------------------------");
   BLE.setEventHandler(BLEDiscovered, blePeripheralDiscoveredHandler);
-    //scanForName("Ruuvi 69E0", true);
-  BLE.scanForAddress("f2:40:f1:bc:69:e0");
+  BLE.scan();
 }
 
 void loop() {
   BLE.poll();
-  
   print_data();
 }
